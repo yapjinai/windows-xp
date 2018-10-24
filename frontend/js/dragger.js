e28ddc6e35d432778bd1c19d29f18df16d830fa4
yapjinai <jinai.jyap@gmail.com>
2018-10-24 10:58:30 -0400

    multiple windows

--- frontend/js/dragger.js
+++ frontend/js/dragger.js
@@ -1,45 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
+function makeDraggable(elmnt, dragger) {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
 
-//////////////////////////////////// make windows draggable
+  dragger.addEventListener('mousedown', dragMouseDown)
 
-  const noteWindow = document.querySelector('.note-window')
-  const dragger = document.querySelector('.dragger')
-
-
-  function dragElement(elmnt) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
-
-    // dragger.onmousedown = dragMouseDown
-    dragger.addEventListener('mousedown', dragMouseDown)
-
-    function dragMouseDown(e) {
-      e.preventDefault()
-      // get the mouse cursor position at startup:
-      pos3 = e.clientX
-      pos4 = e.clientY
-      document.addEventListener('mouseup', closeDragElement)
-      // call a function whenever the cursor moves:
-      document.addEventListener('mousemove', elementDrag)
-    }
-
-    function elementDrag(e) {
-      e.preventDefault()
-      // calculate the new cursor position:
-      pos1 = pos3 - e.clientX
-      pos2 = pos4 - e.clientY
-      pos3 = e.clientX
-      pos4 = e.clientY
-      // set the element's new position:
-      elmnt.style.top = (elmnt.offsetTop - pos2) + "px"
-      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px"
-    }
-
-    function closeDragElement() {
-      /* stop moving when mouse button is released:*/
-      document.removeEventListener('mouseup', closeDragElement)
-      document.removeEventListener('mousemove', elementDrag)
-    }
+  function dragMouseDown(e) {
+    e.preventDefault()
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX
+    pos4 = e.clientY
+    document.addEventListener('mouseup', closeDragElement)
+    // call a function whenever the cursor moves:
+    document.addEventListener('mousemove', elementDrag)
   }
-  dragElement(noteWindow)
 
-})
+  function elementDrag(e) {
+    e.preventDefault()
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX
+    pos2 = pos4 - e.clientY
+    pos3 = e.clientX
+    pos4 = e.clientY
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px"
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px"
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.removeEventListener('mouseup', closeDragElement)
+    document.removeEventListener('mousemove', elementDrag)
+  }
+}
